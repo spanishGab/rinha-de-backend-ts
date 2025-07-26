10e01f74e49b96b62d4727945b21dcc2fde04150
spanishGab <gabrielmail09@gmail.com>
2025-07-26 07:57:06 -0300

    chore: add eslint correct configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,19 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_"
+        }
+      ]
+    }
+  },
   tseslint.configs.recommended,
 ]);
